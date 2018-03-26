7a98d7ba550e28d64047e8be9cbe54690cf41ae4
Tobias Koppers <tobias.koppers@googlemail.com>
2018-03-26 15:57:21 +0200

    add infrastructure for manual in browser testing

---
+++ test/manual/src/index.js
@@ -0,0 +1,22 @@
+import './initial.css';
+
+const handleError = (err) => {
+  document.querySelector(".errors").textContent += `\n${err.toString()}`;
+  console.error(err);
+}
+
+const makeButton = (className, fn) => {
+  const button = document.querySelector(className);
+  button.addEventListener("click", () => {
+    button.disabled = true;
+    fn().then(() => {
+      button.disabled = false;
+    }).catch(handleError);
+  });
+}
+
+makeButton(".lazy-button", () => import('./lazy.js'));
+makeButton(".lazy-button2", () => import('./lazy2.css'));
+
+makeButton(".preloaded-button1", () => import(/* webpackChunkName: "preloaded1" */ './preloaded1'));
+makeButton(".preloaded-button2", () => import(/* webpackChunkName: "preloaded2" */ './preloaded2'));
---
+++ test/manual/src/preloaded1.js
@@ -0,0 +1,3 @@
+import './preloaded1.css';
+
+alert('Ok');
