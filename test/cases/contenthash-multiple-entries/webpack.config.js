8df3ac83d7f19c5e5f130f6030174d819db3f4fc
unchai <unchai@protonmail.com>
2018-06-28 21:07:22 +0900

    test: add contenthash-multiple-entries test code

---
+++ test/cases/contenthash-multiple-entries/webpack.config.js
@@ -0,0 +1,30 @@
+const Self = require('../../../');
+
+module.exports = {
+  entry: {
+    'entryA': './entryA.js',
+    'entryB': './entryB.js',
+    'entryC': './entryC.js',
+    'entryD': './entryD.js',
+    'entryE': './entryE.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          Self.loader,
+          'css-loader',
+        ],
+      },
+    ],
+  },
+  output: {
+    filename: '[name]-[contenthash].js',
+  },
+  plugins: [
+    new Self({
+      filename: '[contenthash].css',
+    }),
+  ],
+};
