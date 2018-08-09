b990ee0f77144694fe254cff91bfb744e72b250c
ShanaMaid <uestczeng@gmail.com>
2018-08-09 10:43:10 +0800

    feat(test): add test

---
+++ test/cases/simple-async-load-css/webpack.config.js
@@ -0,0 +1,23 @@
+const Self = require('../../../');
+
+module.exports = {
+  entry: {
+    'main': './index.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          Self.loader,
+          'css-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new Self({
+      filename: '[name].css',
+    }),
+  ],
+};
