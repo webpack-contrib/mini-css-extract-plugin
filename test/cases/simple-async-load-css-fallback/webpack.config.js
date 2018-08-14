97e2f4cb3114e89cdccdcb57b0b88eca20dd11d9
Jingyi Zeng <uestczeng@gmail.com>
2018-08-14 18:24:01 +0800

    test: loading order (#236)
    * fix css files load order mismatch #188
    
    * fix(src): fix sort bug
    
    * feat(test): add test
    
    * test(sorting): add test case which uses the fallback behavior
    
    * fix(sorting): revert change to restore original behavior
    
    This fixes the testcase

---
+++ test/cases/simple-async-load-css-fallback/webpack.config.js
@@ -0,0 +1,33 @@
+const Self = require('../../../');
+
+module.exports = {
+  entry: {
+    'main': './index.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          Self.loader,
+          'css-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    function() {
+      this.hooks.compilation.tap("Test", compilation => {
+        compilation.hooks.beforeChunkAssets.tap("Test", () => {
+          for (const chunkGroup of compilation.chunkGroups) {
+            // remove getModuleIndex2 to enforce using fallback
+            chunkGroup.getModuleIndex2 = undefined;
+          }
+        });
+      })
+    },
+    new Self({
+      filename: '[name].css',
+    }),
+  ],
+};
