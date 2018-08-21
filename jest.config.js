c671f08e1a778f4c872aaf4021874a60487f4bee
Georgios Giannoutsos Barkas <contact@nuc.gr>
2018-08-21 15:26:23 +0200

    fix(test): provide testEnvironment for jest

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,5 @@
 module.exports = {
-  transformIgnorePatterns: [
-    '/node_modules/',
-    '<rootDir>/dist/',
-  ],
-  watchPathIgnorePatterns: [
-    '<rootDir>/test/js',
-  ],
+  transformIgnorePatterns: ['/node_modules/', '<rootDir>/dist/'],
+  watchPathIgnorePatterns: ['<rootDir>/test/js'],
+  testEnvironment: 'node',
 };
