edaeda7b76b9d931717665cbf17faa8139172ee2
l5oo00 <l5oo00l7@gmail.com>
2018-09-04 11:44:37 +0800

    test(index): add test case for `checkCssChunk` option

---
+++ test/cases/runtime-without-check-csschunks/webpack.config.js
@@ -0,0 +1,22 @@
+const Self = require('../../../');
+
+module.exports = {
+  entry: './index.js',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          Self.loader,
+          'css-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new Self({
+      filename: '[name].css',
+      checkCssChunk: false,
+    }),
+  ],
+};
