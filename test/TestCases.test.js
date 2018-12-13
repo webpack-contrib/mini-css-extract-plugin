7d1e0cad92732627a267759300a73a870782ee29
Evilebot Tnawi <evilebottnawi@users.noreply.github.com>
2018-12-13 18:00:57 +0300

    test: source map (#327)

--- test/TestCases.test.js
+++ test/TestCases.test.js
@@ -9,81 +9,71 @@ describe('TestCases', () => {
   for (const directory of fs.readdirSync(casesDirectory)) {
     if (!/^(\.|_)/.test(directory)) {
       // eslint-disable-next-line no-loop-func
-      it(
-        `${directory} should compile to the expected result`,
-        (done) => {
-          const directoryForCase = path.resolve(casesDirectory, directory);
-          const outputDirectoryForCase = path.resolve(
-            outputDirectory,
-            directory
+      it(`${directory} should compile to the expected result`, (done) => {
+        const directoryForCase = path.resolve(casesDirectory, directory);
+        const outputDirectoryForCase = path.resolve(outputDirectory, directory);
+        // eslint-disable-next-line import/no-dynamic-require, global-require
+        const webpackConfig = require(path.resolve(
+          directoryForCase,
+          'webpack.config.js'
+        ));
+        for (const config of [].concat(webpackConfig)) {
+          Object.assign(
+            config,
+            {
+              mode: 'none',
+              context: directoryForCase,
+              output: Object.assign(
+                {
+                  path: outputDirectoryForCase,
+                },
+                config.output
+              ),
+            },
+            config
           );
-          // eslint-disable-next-line import/no-dynamic-require, global-require
-          const webpackConfig = require(path.resolve(
-            directoryForCase,
-            'webpack.config.js'
-          ));
-          for (const config of [].concat(webpackConfig)) {
-            Object.assign(
-              config,
-              {
-                mode: 'none',
-                context: directoryForCase,
-                output: Object.assign(
-                  {
-                    path: outputDirectoryForCase,
-                  },
-                  config.output
-                ),
-              },
-              config
-            );
+        }
+        webpack(webpackConfig, (err, stats) => {
+          if (err) {
+            done(err);
+            return;
           }
-          webpack(webpackConfig, (err, stats) => {
-            if (err) {
-              done(err);
-              return;
-            }
-            done();
-            // eslint-disable-next-line no-console
-            console.log(
-              stats.toString({
-                context: path.resolve(__dirname, '..'),
-                chunks: true,
-                chunkModules: true,
-                modules: false,
-              })
+          done();
+          // eslint-disable-next-line no-console
+          console.log(
+            stats.toString({
+              context: path.resolve(__dirname, '..'),
+              chunks: true,
+              chunkModules: true,
+              modules: false,
+            })
+          );
+          if (stats.hasErrors()) {
+            done(
+              new Error(
+                stats.toString({
+                  context: path.resolve(__dirname, '..'),
+                  errorDetails: true,
+                })
+              )
             );
-            if (stats.hasErrors()) {
-              done(
-                new Error(
-                  stats.toString({
-                    context: path.resolve(__dirname, '..'),
-                    errorDetails: true,
-                  })
-                )
-              );
-              return;
-            }
-            const expectedDirectory = path.resolve(
-              directoryForCase,
-              'expected'
+            return;
+          }
+          const expectedDirectory = path.resolve(directoryForCase, 'expected');
+          for (const file of fs.readdirSync(expectedDirectory)) {
+            const content = fs.readFileSync(
+              path.resolve(expectedDirectory, file),
+              'utf-8'
             );
-            for (const file of fs.readdirSync(expectedDirectory)) {
-              const content = fs.readFileSync(
-                path.resolve(expectedDirectory, file),
-                'utf-8'
-              );
-              const actualContent = fs.readFileSync(
-                path.resolve(outputDirectoryForCase, file),
-                'utf-8'
-              );
-              expect(actualContent).toEqual(content);
-            }
-            done();
-          });
-        },
-        10000
-      );
+            const actualContent = fs.readFileSync(
+              path.resolve(outputDirectoryForCase, file),
+              'utf-8'
+            );
+            expect(actualContent).toEqual(content);
+          }
+          done();
+        });
+      }, 10000);
     }
   }
 });
