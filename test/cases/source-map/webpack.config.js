905a2e22e3513aa4aa95ab5e2a8227a02feb5989
evilebottnawi <sheo13666q@gmail.com>
2018-12-13 17:50:12 +0300

    test: source map

---
+++ test/cases/source-map/webpack.config.js
@@ -0,0 +1,32 @@
+const Self = require('../../../');
+
+module.exports = {
+  entry: './index.js',
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: Self.loader,
+            options: {
+              sourceMap: true
+            }
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true
+            }
+          }
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new Self({
+      filename: '[name].css',
+    }),
+  ],
+};
