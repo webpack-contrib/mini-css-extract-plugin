07bd40eec209cb71e16f3be844294cee7e61cb05
Naoise Golden Santos <naoise.golden@gmail.com>
2018-12-18 18:11:02 +0100

    Update src/loader.js
    Co-Authored-By: MoOx <git@moox.io>

--- src/loader.js
+++ src/loader.js
@@ -125,7 +125,7 @@ export function pitch(request) {
         });
       }
       if(typeof this[MODULE_TYPE] !== 'function') {
-        return callback(new Error("It seems you are using the loader but forgot to register the plugin"));
+        return callback(new Error('It seems you are using the loader but forgot to register the plugin'));
       }
       this[MODULE_TYPE](text);
     } catch (e) {
