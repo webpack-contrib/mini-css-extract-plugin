1b691abf23ef62c45888eba50687b4e282baca65
Karl von Randow <karl@xk72.com>
2019-04-06 07:27:56 +1300

    fix: publicpath-function test demonstrates variable publicPath
    Added a second entry point / output at different nesting demonstrating the application of the publicPath function

--- test/cases/publicpath-function/webpack.config.js
+++ test/cases/publicpath-function/webpack.config.js
@@ -5,6 +5,8 @@ module.exports = {
   entry: {
     // Specific CSS entry point, with output to a nested folder
     'nested/style': './nested/style.css',
+    // Note that relative nesting of output is the same as that of the input
+    'nested/again/style': './nested/again/style.css',
   },
   module: {
     rules: [
