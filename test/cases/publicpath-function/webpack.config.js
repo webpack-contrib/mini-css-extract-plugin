7b1425a2e4578cd9017a124f010ee3bf80fe7789
Karl von Randow <karl@xk72.com>
2019-04-09 04:09:40 +1200

    feat: publicPath can be a function (#373)

---
+++ test/cases/publicpath-function/webpack.config.js
@@ -0,0 +1,41 @@
+const Self = require('../../../');
+const path = require('path')
+
+module.exports = {
+  entry: {
+    // Specific CSS entry point, with output to a nested folder
+    'nested/style': './nested/style.css',
+    // Note that relative nesting of output is the same as that of the input
+    'nested/again/style': './nested/again/style.css',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: Self.loader,
+            options: {
+              // Compute publicPath relative to the CSS output
+              publicPath: (resourcePath, context) => path.relative(path.dirname(resourcePath), context) + '/',
+            }
+          },
+          'css-loader',
+        ],
+      }, {
+        test: /\.(svg|png)$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            filename: '[name].[ext]'
+          }
+        }]
+      }
+    ],
+  },
+  plugins: [
+    new Self({
+      filename: '[name].css',
+    }),
+  ],
+};
