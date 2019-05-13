7d022b3b7c39db965fb3f154559a9db838e6343c
Evilebot Tnawi <evilebottnawi@users.noreply.github.com>
2019-05-13 17:19:55 +0300

    chore(defaults): update (#395)

---
+++ babel.config.js
@@ -0,0 +1,19 @@
+const MIN_BABEL_VERSION = 7;
+
+module.exports = (api) => {
+  api.assertVersion(MIN_BABEL_VERSION);
+  api.cache(true);
+
+  return {
+    presets: [
+      [
+        '@babel/preset-env',
+        {
+          targets: {
+            node: '6.9.0',
+          },
+        },
+      ],
+    ],
+  };
+};
