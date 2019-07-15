98de7b43f54a8db4c06bafa4bf7852e11ba81895
Matt Henkes <mjhenkes@gmail.com>
2019-07-15 09:48:29 -0500

    feat: moar tests

---
+++ test/IgnoreOrder.test.js
@@ -0,0 +1,44 @@
+import path from 'path';
+import webpack from 'webpack';
+
+describe('IgnoreOrder', () => {
+  it('should emit errors', (done) => {
+    const casesDirectory = path.resolve(__dirname, 'cases');
+    const directoryForCase = path.resolve(casesDirectory, 'ignoreOrderFalse');
+    // eslint-disable-next-line import/no-dynamic-require, global-require
+    const webpackConfig = require(path.resolve(
+      directoryForCase,
+      'webpack.config.js'
+    ));
+    const compiler = webpack({
+      ...webpackConfig,
+      mode: 'development',
+      context: directoryForCase,
+      cache: false,
+    });
+    compiler.run((err1, stats) => {
+      expect(stats.hasWarnings()).toBeTruthy();
+      done();
+    });
+  });
+
+  it('should not emit errors', (done) => {
+    const casesDirectory = path.resolve(__dirname, 'cases');
+    const directoryForCase = path.resolve(casesDirectory, 'ignoreOrder');
+    // eslint-disable-next-line import/no-dynamic-require, global-require
+    const webpackConfig = require(path.resolve(
+      directoryForCase,
+      'webpack.config.js'
+    ));
+    const compiler = webpack({
+      ...webpackConfig,
+      mode: 'development',
+      context: directoryForCase,
+      cache: false,
+    });
+    compiler.run((err1, stats) => {
+      expect(stats.hasWarnings()).toBeFalsy();
+      done();
+    });
+  });
+});
