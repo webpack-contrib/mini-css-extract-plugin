c38cc8c5662326208533a3d3e25a6a6840d5bb9c
Light Leung <lsycxyj@gmail.com>
2019-07-21 14:10:29 +0800

    feat: ability to disable extract async modules, add tests for js files

---
+++ test/cases/disable-async-bool/expected/2.js
@@ -0,0 +1,10 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[2],{
+
+/***/ 7:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = ".async {\n  background: blue;\n}\n";
+
+/***/ })
+
+}]);
---
+++ test/cases/disable-async-func/expected/3.js
@@ -0,0 +1,10 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[3],{
+
+/***/ 10:
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+
+/***/ })
+
+}]);
