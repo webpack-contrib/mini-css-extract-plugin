164a062c4411894ea9367a6a29d545f726ca2bf2
TotooriaHyperion <shr9192@gmail.com>
2020-03-25 18:01:48 +0800

    chore: disable eslint in manual test for hmr test

--- test/manual/src/hmr-module.js
+++ test/manual/src/hmr-module.js
@@ -3,6 +3,7 @@ import style from './hmr-module.module.css';
 
 export default function hmrBootstrap() {
   // should not log again if only css changed
+  // eslint-disable-next-line
   console.log('load');
   document.querySelector('.hmr-a').classList.add(style.a);
 }
