41ed56c5e22a5072e95f23dbd074807b0ce3cef8
evilebottnawi <sheo13666q@gmail.com>
2020-09-04 19:20:25 +0300

    test: fix

--- test/TestCache.test.js
+++ test/TestCache.test.js
@@ -16,70 +16,74 @@ del.sync(fileSystemCacheDirectory);
 
 describe('TestCache', () => {
   it('should work', async () => {
-    const casesDirectory = path.resolve(__dirname, 'cases');
-    const directoryForCase = path.resolve(casesDirectory, 'simple');
-    // eslint-disable-next-line import/no-dynamic-require, global-require
-    const webpackConfig = require(path.resolve(
-      directoryForCase,
-      'webpack.config.js'
-    ));
+    if (webpack.version[0] !== '4') {
+      const casesDirectory = path.resolve(__dirname, 'cases');
+      const directoryForCase = path.resolve(casesDirectory, 'simple');
+      // eslint-disable-next-line import/no-dynamic-require, global-require
+      const webpackConfig = require(path.resolve(
+        directoryForCase,
+        'webpack.config.js'
+      ));
 
-    const compiler1 = webpack({
-      ...webpackConfig,
-      mode: 'development',
-      context: directoryForCase,
-      cache: {
-        type: 'filesystem',
-        cacheDirectory: fileSystemCacheDirectory,
-        idleTimeout: 0,
-        idleTimeoutForInitialStore: 0,
-      },
-    });
+      const compiler1 = webpack({
+        ...webpackConfig,
+        mode: 'development',
+        context: directoryForCase,
+        cache: {
+          type: 'filesystem',
+          cacheDirectory: fileSystemCacheDirectory,
+          idleTimeout: 0,
+          idleTimeoutForInitialStore: 0,
+        },
+      });
 
-    await new Promise((resolve, reject) => {
-      compiler1.run((error, stats) => {
-        if (error) {
-          reject(error);
+      await new Promise((resolve, reject) => {
+        compiler1.run((error, stats) => {
+          if (error) {
+            reject(error);
 
-          return;
-        }
+            return;
+          }
 
-        expect(stats.compilation.warnings).toHaveLength(0);
-        expect(stats.compilation.errors).toHaveLength(0);
+          expect(stats.compilation.warnings).toHaveLength(0);
+          expect(stats.compilation.errors).toHaveLength(0);
 
-        compiler1.close(() => {
-          resolve();
+          compiler1.close(() => {
+            resolve();
+          });
         });
       });
-    });
 
-    const compiler2 = webpack({
-      ...webpackConfig,
-      mode: 'development',
-      context: directoryForCase,
-      cache: {
-        type: 'filesystem',
-        cacheDirectory: fileSystemCacheDirectory,
-        idleTimeout: 0,
-        idleTimeoutForInitialStore: 0,
-      },
-    });
+      const compiler2 = webpack({
+        ...webpackConfig,
+        mode: 'development',
+        context: directoryForCase,
+        cache: {
+          type: 'filesystem',
+          cacheDirectory: fileSystemCacheDirectory,
+          idleTimeout: 0,
+          idleTimeoutForInitialStore: 0,
+        },
+      });
 
-    await new Promise((resolve, reject) => {
-      compiler2.run((error, stats) => {
-        if (error) {
-          reject(error);
+      await new Promise((resolve, reject) => {
+        compiler2.run((error, stats) => {
+          if (error) {
+            reject(error);
 
-          return;
-        }
+            return;
+          }
 
-        expect(stats.compilation.warnings).toHaveLength(0);
-        expect(stats.compilation.errors).toHaveLength(0);
+          expect(stats.compilation.warnings).toHaveLength(0);
+          expect(stats.compilation.errors).toHaveLength(0);
 
-        compiler2.close(() => {
-          resolve();
+          compiler2.close(() => {
+            resolve();
+          });
         });
       });
-    });
+    } else {
+      expect(true).toBe(true);
+    }
   });
 });
