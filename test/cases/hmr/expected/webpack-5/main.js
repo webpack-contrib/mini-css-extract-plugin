ef06d6bb3fcf7ff80307bf295a04004c8291e4f4
cap-Bernardito <cap-Bernardito@yandex.ru>
2020-10-17 20:14:31 +0300

    refactor: drop normalize-url package

--- test/cases/hmr/expected/webpack-5/main.js
+++ test/cases/hmr/expected/webpack-5/main.js
@@ -255,9 +255,40 @@ module.exports = function (moduleId, options) {
 
 /* eslint-disable */
 
-module.exports = function (urlString) {
-  var defaultProtocol = 'http:';
+function normalizeUrl(pathComponents) {
+  var result = [];
 
+  pathComponents.forEach((item) => {
+    switch (item) {
+      case '..':
+        result.pop();
+        break;
+      case '.':
+        break;
+      default:
+        result.push(item);
+    }
+  });
+
+  return result.join('/');
+}
+
+var parseUrl = function (url) {
+  var protocol =
+    url.indexOf('//') !== -1 ? url.split('//')[0] + '//' : 'http://';
+  var components = url.replace(new RegExp(protocol, 'i'), '').split('/');
+  var host = components[0];
+
+  components[0] = '';
+
+  return {
+    protocol: protocol,
+    host: host.toLowerCase(),
+    path: normalizeUrl(components),
+  };
+};
+
+module.exports = function (urlString) {
   urlString = urlString.trim();
 
   if (/^data:/i.test(urlString)) {
@@ -266,32 +297,20 @@ module.exports = function (urlString) {
 
   var hasRelativeProtocol =
     urlString.length > 2 && urlString[0] === '/' && urlString[1] === '/';
-  var isRelativeUrl = !hasRelativeProtocol && /^\.*\//.test(urlString);
 
-  if (!isRelativeUrl) {
-    urlString = urlString.replace(/^(?!(?:\w+:)?\/\/)|^\/\//, defaultProtocol);
-  }
-
-  var urlObj = new URL(urlString);
+  var urlObj = parseUrl(urlString);
 
   var keepTrailingSlash = false;
 
-  // Remove duplicate slashes if not preceded by a protocol
-  if (urlObj.pathname) {
-    urlObj.pathname = urlObj.pathname.replace(
-      /(?<!\b(?:[a-z][a-z\d+\-.]{1,50}:))\/{2,}/g,
-      '/'
-    );
-
-    keepTrailingSlash = /\/$/i.test(urlString) && /\/$/i.test(urlObj.pathname);
+  if (urlObj.path) {
+    keepTrailingSlash = /\/$/i.test(urlString) && /\/$/i.test(urlObj.path);
   }
 
-  if (urlObj.hostname) {
-    urlObj.hostname = urlObj.hostname.replace(/\.$/, '');
+  if (urlObj.host) {
+    urlObj.host = urlObj.host.replace(/\.$/, '');
   }
 
-  // Take advantage of many of the Node `url` normalizations
-  urlString = urlObj.toString();
+  urlString = urlObj.protocol + urlObj.host + urlObj.path;
 
   if (!keepTrailingSlash && urlObj.hash === '') {
     urlString = urlString.replace(/\/$/, '');
