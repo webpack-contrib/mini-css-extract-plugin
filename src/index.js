0dc9b6953242816d36bdb1026a96eab045d655b7
evilebottnawi <sheo13666q@gmail.com>
2021-02-10 00:58:29 +0300

    fix: webpack 5 support

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@
 
 import { validate } from 'schema-utils';
 
-import CssModule from './CssModule';
 import CssDependency from './CssDependency';
 import schema from './plugin-options.json';
 import { MODULE_TYPE, compareModulesByIdentifier } from './utils';
@@ -15,6 +14,14 @@ const REGEXP_NAME = /\[name\]/i;
 const DEFAULT_FILENAME = '[name].css';
 
 const pathLength = `${pluginName}/dist`.length;
+
+const TYPES = new Set([MODULE_TYPE]);
+const CODE_GENERATION_RESULT = {
+  sources: new Map(),
+  runtimeRequirements: new Set(),
+};
+
+const registeredSerializers = new Set();
 
 class MiniCssExtractPlugin {
   constructor(options = {}) {
@@ -70,10 +77,124 @@ class MiniCssExtractPlugin {
       : // eslint-disable-next-line global-require
         require('webpack');
 
+    class CssModule extends webpack.Module {
+      constructor({
+        context,
+        identifier,
+        identifierIndex,
+        content,
+        media,
+        sourceMap,
+        assets,
+        assetsInfo,
+      }) {
+        super(MODULE_TYPE, context);
+
+        this.id = '';
+        this._context = context;
+        this._identifier = identifier;
+        this._identifierIndex = identifierIndex;
+        this.content = content;
+        this.media = media;
+        this.sourceMap = sourceMap;
+        this.buildInfo = {
+          assets,
+          assetsInfo,
+        };
+        this.buildMeta = {};
+      }
+
+      // no source() so webpack 4 doesn't do add stuff to the bundle
+
+      size() {
+        return this.content.length;
+      }
+
+      identifier() {
+        return `css|${this._identifier}|${this._identifierIndex}`;
+      }
+
+      readableIdentifier(requestShortener) {
+        return `css ${requestShortener.shorten(this._identifier)}${
+          this._identifierIndex ? ` (${this._identifierIndex})` : ''
+        }`;
+      }
+
+      // eslint-disable-next-line class-methods-use-this
+      getSourceTypes() {
+        return TYPES;
+      }
+
+      // eslint-disable-next-line class-methods-use-this
+      codeGeneration() {
+        return CODE_GENERATION_RESULT;
+      }
+
+      nameForCondition() {
+        const resource = this._identifier.split('!').pop();
+        const idx = resource.indexOf('?');
+
+        if (idx >= 0) {
+          return resource.substring(0, idx);
+        }
+
+        return resource;
+      }
+
+      updateCacheModule(module) {
+        this.content = module.content;
+        this.media = module.media;
+        this.sourceMap = module.sourceMap;
+      }
+
+      // eslint-disable-next-line class-methods-use-this
+      needRebuild() {
+        return true;
+      }
+
+      // eslint-disable-next-line class-methods-use-this
+      needBuild(context, callback) {
+        callback(null, false);
+      }
+
+      build(options, compilation, resolver, fileSystem, callback) {
+        this.buildInfo = {};
+        this.buildMeta = {};
+
+        callback();
+      }
+
+      updateHash(hash, context) {
+        super.updateHash(hash, context);
+
+        hash.update(this.content);
+        hash.update(this.media || '');
+        hash.update(this.sourceMap ? JSON.stringify(this.sourceMap) : '');
+      }
+
+      serialize(context) {
+        const { write } = context;
+
+        write(this._context);
+        write(this._identifier);
+        write(this._identifierIndex);
+        write(this.content);
+        write(this.media);
+        write(this.sourceMap);
+        write(this.buildInfo);
+
+        super.serialize(context);
+      }
+
+      deserialize(context) {
+        super.deserialize(context);
+      }
+    }
+
     if (
       webpack.util &&
       webpack.util.serialization &&
-      webpack.util.serialization.registerLoader
+      webpack.util.serialization.register
     ) {
       webpack.util.serialization.registerLoader(
         /^mini-css-extract-plugin\//,
@@ -84,6 +205,47 @@ class MiniCssExtractPlugin {
           return true;
         }
       );
+
+      if (!registeredSerializers.has('CssModule')) {
+        registeredSerializers.add('CssModule');
+
+        webpack.util.serialization.register(
+          CssModule,
+          'mini-css-extract-plugin/dist',
+          'CssModule',
+          {
+            serialize(instance, context) {
+              instance.serialize(context);
+            },
+            deserialize(context) {
+              const { read } = context;
+
+              const contextModule = read();
+              const identifier = read();
+              const identifierIndex = read();
+              const content = read();
+              const media = read();
+              const sourceMap = read();
+              const { assets, assetsInfo } = read();
+
+              const dep = new CssModule({
+                context: contextModule,
+                identifier,
+                identifierIndex,
+                content,
+                media,
+                sourceMap,
+                assets,
+                assetsInfo,
+              });
+
+              dep.deserialize(context);
+
+              return dep;
+            },
+          }
+        );
+      }
     }
 
     const isWebpack4 = compiler.webpack
