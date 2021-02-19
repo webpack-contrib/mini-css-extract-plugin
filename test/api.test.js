8bdcfead03070c78c8ffcbb4f1d5b0a25b843e07
Barak Igal <baraki@wix.com>
2021-02-19 15:17:45 +0200

    use imported webpack in api test

--- test/api.test.js
+++ test/api.test.js
@@ -1,10 +1,11 @@
+import webpack from 'webpack';
+
 import MiniCssExtractPlugin from '../src';
 
-import { getCompiler } from './helpers'
+
 
 describe('API', () => {
   it('should return the same CssModule when same webpack instance provided', () => {
-    const { webpack } = getCompiler('');
     expect(MiniCssExtractPlugin.getCssModule(webpack)).toEqual(MiniCssExtractPlugin.getCssModule(webpack));
   });
 });
