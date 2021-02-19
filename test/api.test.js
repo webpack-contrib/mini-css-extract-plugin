88b9917944469f99a2c40b0b8a5fb2f1ed731dd6
Barak Igal <baraki@wix.com>
2021-02-19 15:18:57 +0200

    remove empty space

--- test/api.test.js
+++ test/api.test.js
@@ -2,8 +2,6 @@ import webpack from 'webpack';
 
 import MiniCssExtractPlugin from '../src';
 
-
-
 describe('API', () => {
   it('should return the same CssModule when same webpack instance provided', () => {
     expect(MiniCssExtractPlugin.getCssModule(webpack)).toEqual(MiniCssExtractPlugin.getCssModule(webpack));
