b6ab076f703e1c3f150ccce973027e35aed8b1fa
evilebottnawi <sheo13666q@gmail.com>
2021-06-30 19:15:19 +0300

    refactor: reduce deps

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,5 @@
 import NativeModule from 'module';
+import path from 'path';
 
 function trueFn() {
   return true;
@@ -54,6 +55,50 @@ function compareModulesByIdentifier(a, b) {
 const MODULE_TYPE = 'css/mini-extract';
 const AUTO_PUBLIC_PATH = '__MINI_CSS_EXTRACT_PLUGIN_PUBLIC_PATH__';
 
+function isAbsolutePath(str) {
+  return path.posix.isAbsolute(str) || path.win32.isAbsolute(str);
+}
+
+const RELATIVE_PATH_REGEXP = /^\.\.?[/\\]/;
+
+function isRelativePath(str) {
+  return RELATIVE_PATH_REGEXP.test(str);
+}
+
+function stringifyRequest(loaderContext, request) {
+  const splitted = request.split('!');
+  const { context } = loaderContext;
+
+  return JSON.stringify(
+    splitted
+      .map((part) => {
+        // First, separate singlePath from query, because the query might contain paths again
+        const splittedPart = part.match(/^(.*?)(\?.*)/);
+        const query = splittedPart ? splittedPart[2] : '';
+        let singlePath = splittedPart ? splittedPart[1] : part;
+
+        if (isAbsolutePath(singlePath) && context) {
+          singlePath = path.relative(context, singlePath);
+
+          if (isAbsolutePath(singlePath)) {
+            // If singlePath still matches an absolute path, singlePath was on a different drive than context.
+            // In this case, we leave the path platform-specific without replacing any separators.
+            // @see https://github.com/webpack/loader-utils/pull/14
+            return singlePath + query;
+          }
+
+          if (isRelativePath(singlePath) === false) {
+            // Ensure that the relative path starts at least with ./ otherwise it would be a request into the modules directory (like node_modules).
+            singlePath = `./${singlePath}`;
+          }
+        }
+
+        return singlePath.replace(/\\/g, '/') + query;
+      })
+      .join('!')
+  );
+}
+
 export {
   trueFn,
   findModuleById,
@@ -61,4 +106,5 @@ export {
   compareModulesByIdentifier,
   MODULE_TYPE,
   AUTO_PUBLIC_PATH,
+  stringifyRequest,
 };
