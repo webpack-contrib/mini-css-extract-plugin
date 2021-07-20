f6101a488cbc21a6f0ba6797a7c4bca712203496
Alexander Akait <4567934+alexander-akait@users.noreply.github.com>
2021-07-20 21:44:45 +0300

    docs: fix webpack version (#791)

--- test/cases/runtime/expected/runtime~main.js
+++ test/cases/runtime/expected/runtime~main.js
@@ -54,7 +54,8 @@
 /******/ 				}
 /******/ 				if(fulfilled) {
 /******/ 					deferred.splice(i--, 1)
-/******/ 					result = fn();
+/******/ 					var r = fn();
+/******/ 					if (r !== undefined) result = r;
 /******/ 				}
 /******/ 			}
 /******/ 			return result;
