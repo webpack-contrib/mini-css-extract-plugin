7c98bdb470aa728f0d54a0618ec786ddbfc3d4b8
evilebottnawi <sheo13666q@gmail.com>
2021-09-02 19:08:04 +0300

    test: fix

--- test/cases/es-named-export-output-module/expected/main.mjs
+++ test/cases/es-named-export-output-module/expected/main.mjs
@@ -79,7 +79,7 @@ __webpack_require__.r(__webpack_exports__);
 
 
 // eslint-disable-next-line no-console
-console.log({ css: _style_css__WEBPACK_IMPORTED_MODULE_0__.default, aClass: _style_css__WEBPACK_IMPORTED_MODULE_0__.aClass, bClass: _style_css__WEBPACK_IMPORTED_MODULE_0__.bClass, cClass: _style_css__WEBPACK_IMPORTED_MODULE_0__.cClass });
+console.log({ css: _style_css__WEBPACK_IMPORTED_MODULE_0__["default"], aClass: _style_css__WEBPACK_IMPORTED_MODULE_0__.aClass, bClass: _style_css__WEBPACK_IMPORTED_MODULE_0__.bClass, cClass: _style_css__WEBPACK_IMPORTED_MODULE_0__.cClass });
 
 })();
 
