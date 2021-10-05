b4431cb60a6eadcf8c2b614f494faf899c73aaa0
Alexander Akait <4567934+alexander-akait@users.noreply.github.com>
2021-10-05 23:54:42 +0300

    fix: crash with multiple webpack versions (#845)

--- src/utils.js
+++ src/utils.js
@@ -102,6 +102,45 @@ function stringifyRequest(loaderContext, request) {
   );
 }
 
+function getUndoPath(filename, outputPath, enforceRelative) {
+  let depth = -1;
+  let append = "";
+
+  // eslint-disable-next-line no-param-reassign
+  outputPath = outputPath.replace(/[\\/]$/, "");
+
+  for (const part of filename.split(/[/\\]+/)) {
+    if (part === "..") {
+      if (depth > -1) {
+        // eslint-disable-next-line no-plusplus
+        depth--;
+      } else {
+        const i = outputPath.lastIndexOf("/");
+        const j = outputPath.lastIndexOf("\\");
+        const pos = i < 0 ? j : j < 0 ? i : Math.max(i, j);
+
+        if (pos < 0) {
+          return `${outputPath}/`;
+        }
+
+        append = `${outputPath.slice(pos + 1)}/${append}`;
+
+        // eslint-disable-next-line no-param-reassign
+        outputPath = outputPath.slice(0, pos);
+      }
+    } else if (part !== ".") {
+      // eslint-disable-next-line no-plusplus
+      depth++;
+    }
+  }
+
+  return depth > 0
+    ? `${"../".repeat(depth)}${append}`
+    : enforceRelative
+    ? `./${append}`
+    : append;
+}
+
 export {
   trueFn,
   findModuleById,
@@ -112,4 +151,5 @@ export {
   ABSOLUTE_PUBLIC_PATH,
   SINGLE_DOT_PATH_SEGMENT,
   stringifyRequest,
+  getUndoPath,
 };
