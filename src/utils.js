499f0c6e5e2512be292b184fd5ec901047829474
evilebottnawi <sheo13666q@gmail.com>
2021-10-21 16:38:00 +0300

    refactor: code

--- src/utils.js
+++ src/utils.js
@@ -68,7 +68,17 @@ function isRelativePath(str) {
   return RELATIVE_PATH_REGEXP.test(str);
 }
 
+// TODO simplify for the next major release
 function stringifyRequest(loaderContext, request) {
+  if (
+    typeof loaderContext.utils !== "undefined" &&
+    typeof loaderContext.utils.contextify === "function"
+  ) {
+    return JSON.stringify(
+      loaderContext.utils.contextify(loaderContext.context, request)
+    );
+  }
+
   const splitted = request.split("!");
   const { context } = loaderContext;
 
