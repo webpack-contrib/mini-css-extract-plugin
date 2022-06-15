3fb68457ac3fdebcbda39434b01a0d9e275efac0
Alexander Akait <4567934+alexander-akait@users.noreply.github.com>
2022-06-15 22:10:50 +0300

    chore(deps): update (#954)

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,4 @@
 module.exports = {
-  testURL: "http://localhost/",
   transformIgnorePatterns: ["/node_modules/", "<rootDir>/dist/"],
   watchPathIgnorePatterns: ["<rootDir>/test/js"],
   setupFilesAfterEnv: ["<rootDir>/setupTest.js"],
