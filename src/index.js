d352d915790cb047ade7219d80198e31762ca7e0
alexander.akait <sheo13666q@gmail.com>
2024-01-10 18:42:25 +0300

    fix: improve

--- src/index.js
+++ src/index.js
@@ -876,9 +876,9 @@ class MiniCssExtractPlugin {
                   Template.indent(["resolve();"]),
                   "} else {",
                   Template.indent([
-                    "var errorType = event && (event.type === 'load' ? 'missing' : event.type);",
+                    "var errorType = event && event.type;",
                     "var realHref = event && event.target && event.target.href || fullhref;",
-                    'var err = new Error("Loading CSS chunk " + chunkId + " failed.\\n(" + realHref + ")");',
+                    'var err = new Error("Loading CSS chunk " + chunkId + " failed.\\n(" + errorType + ": " + realHref + ")");',
                     'err.name = "ChunkLoadError";',
                     'err.code = "CSS_CHUNK_LOAD_FAILED";',
                     "err.type = errorType;",
