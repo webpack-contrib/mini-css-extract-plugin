8f77e19a89eca97b05f1855d2c851592e98ff774
HHX <57352881+houhongxu@users.noreply.github.com>
2024-08-19 06:42:37 -0700

    fix: add `export default {}` when CSS modules enabled and a file is empty for the `defaultExport` option

---
+++ test/cases/es-named-and-default-export-1/webpack.config.js
@@ -0,0 +1,34 @@
+import Self from "../../../src";
+
+module.exports = {
+  entry: "./index.js",
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: Self.loader,
+            options: {
+              defaultExport: true,
+            },
+          },
+          {
+            loader: "css-loader",
+            options: {
+              esModule: true,
+              modules: {
+                namedExport: true,
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new Self({
+      filename: "[name].css",
+    }),
+  ],
+};
